87e6bb7766f75d91340ae4b1ad79c50f59adece8
Charding96 <atsutomo@hotmail.com>
2018-05-07 19:43:32 -0400

    Cook's webpage

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,7 @@ router.use('/login-signup', require('./login-signup'));
 router.use('/feedback', require('./feedback'));
 router.use('/pizza-order', require('./pizza-order'));
 router.use('/aboutus', require('./aboutus'));
+router.use('/cook',require('./cook'));
 router.use('/order-confirmation', require('./order-confirmation'));
 
 
